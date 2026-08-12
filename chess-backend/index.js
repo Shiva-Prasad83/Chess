a913d0651490ba35df6e38c01302ace30c529608
Shiva Prasad <shivaprasad.ch83@gmail.com>
2026-08-12 11:54:54 +0530

    changes for cors options done

--- chess-backend/index.js
+++ chess-backend/index.js
@@ -20,17 +20,9 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
-const allowedOrigins = [
-    "http://localhost:5173",
-    process.env.CLIENT_URL
-].filter(Boolean);
-// app.use(cors({
-//     origin: ["http://localhost:5173",
-//         process.env.CLIENT_URL],
-//     credentials: true
-// }))
+
 app.use(cors({
-    origin: allowedOrigins,
+    origin: [process.env.CLIENT_URL, "http://localhost:5173"],
     credentials: true
 }));
 app.use(express.json());
@@ -57,13 +49,8 @@ app.use('/user', userRouter);
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    // cors: {
-    //     origin: ["http://localhost:5173",
-    //         process.env.CLIENT_URL],
-    //     credentials: true
-    // }
     cors: {
-        origin: allowedOrigins,
+        origin: [process.env.CLIENT_URL, "http://localhost:5173"],
         credentials: true
     }
 })
